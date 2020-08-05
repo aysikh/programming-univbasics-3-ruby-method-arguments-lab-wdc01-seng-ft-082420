2c3ec0ca5287e1b5d40a26f6366c6bbe87c5cac5
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-05 04:00:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ introduction("Josh")
 introduction("Dan")
 
 def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Dan", "Ember.js")
@@ -17,7 +17,7 @@ def introduction_with_language_optional(name,language = "Ruby")
 end
 
 introduction_with_language_optional("Edwin")
-introduction_with_language_optional("Ediwin", "Python")
+introduction_with_language_optional("Edwin", "Python")
 
 
 
